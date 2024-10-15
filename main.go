0b6d06bee5e18578a891ce5b33adf94e5ab9e2d4
eko-prasetio <caedablerg@gmail.com>
2024-10-15 22:26:55 +0700

    service login

--- main.go
+++ main.go
@@ -14,11 +14,12 @@ func main() {
 	dsn := "root:@tcp(127.0.0.1:3306)/go_startup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 	}
-
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
+
+
 
 	userHandler := handler.NewUserHandler(userService)
 
@@ -26,5 +27,6 @@ func main() {
 	api := router.Group("/api/v1")
 
 	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.Login)
 	router.Run(":8088")
 }
--- user/service.go
+++ user/service.go
@@ -1,9 +1,14 @@
 package user
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
+	Login(input LoginInput) (User, error)
 }
 
 type service struct {
@@ -27,11 +32,34 @@ func (s *service) RegisterUser(input RegisterUserInput) (User, error) {
 	user.PasswordHash = string(passwordHash)
 	user.Role = "user"
 
-	newUser, er := s.repository.Save(user)
+	newUser, err := s.repository.Save(user)
 
-	if er != nil {
-		return newUser, er
+	if err != nil {
+		return newUser, err
 	}
 	return newUser, nil
 
 }
+
+func (s *service) Login(input LoginInput) (User, error) {
+	email := input.Email
+	password := input.Password
+
+	user, err := s.repository.FindByEmail(email)
+
+	if err != nil {
+		return user, err
+	}
+	if user.ID == 0 {
+		return user, errors.New("no user found on that email")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
